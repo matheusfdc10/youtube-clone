9ceb45ffc925ac187ef5a4e8a74726c421ce5651
Matheus Freitas <matheusfdc10@hotmail.com>
2025-04-04 14:46:04 -0300

    create banner upload

--- src/modules/users/ui/components/user-page-banner.tsx
+++ src/modules/users/ui/components/user-page-banner.tsx
@@ -4,6 +4,8 @@ import { useAuth } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { Edit2Icon } from "lucide-react";
 import { Skeleton } from "@/components/ui/skeleton";
+import { BannerUploadModal } from "./banner-upload-modal";
+import { useState } from "react";
 
 interface UserPageBannerProps {
     user: UserGetOneOutPut;
@@ -11,9 +13,15 @@ interface UserPageBannerProps {
 
 export const UserPageBanner = ({ user }: UserPageBannerProps) => {
     const { userId } = useAuth();
+    const [isBannerUploaderModal, setIsBannerUploaderModal] = useState(false);
     
     return (
         <div className="relative group">
+            <BannerUploadModal 
+                userId={user.id}
+                open={isBannerUploaderModal}
+                onOpenChange={setIsBannerUploaderModal}
+            />
             <div 
                 className={cn(
                     "w-full max-h-[200px] h-[15vh] md:h-[25vh] bg-gradient-to-r from-gray-100 to-gray-200 rounded-xl",
@@ -27,6 +35,7 @@ export const UserPageBanner = ({ user }: UserPageBannerProps) => {
             >
                 {user.clerkId === userId && (
                     <Button
+                        onClick={() => setIsBannerUploaderModal(true)}
                         type="button"
                         size="icon"
                         className="absolute top-4 right-4 rounded-full bg-black/50 hover:bg-black/50 opacity-100 md:opacity-0 group-hover:opacity-100 transition-opacity duration-300"
